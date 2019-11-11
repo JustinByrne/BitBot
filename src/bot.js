17e6289e323fec40c9b97038ed72cae1afd2bba6
Justin Byrne <theskaterjesus@gmail.com>
2019-11-11 23:42:10 +0000

    Ability for commands to run dynamically closes #1

--- src/bot.js
+++ src/bot.js
@@ -8,7 +8,7 @@ client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-    const command = require('./commands/${file}');
+    const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
 }
 
@@ -20,22 +20,18 @@ client.on('message', (message) => {
     // Preventing bot from replying to another bot or not a command
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    if (message.content.startsWith(prefix))    {
-        processCommand(message);
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    // A command was issued but not a valid one
+    if (!client.commands.has(command)) return;
+
+    try {
+        client.commands.get(command).execute(message, args);
+    } catch (error) {
+        console.log(error);
+        message.reply('It looks like something went horribly wrong! Sorry.');
     }
 })
-
-function processCommand(message)    {
-    let fullCommand = message.content.substr(1); // Removing the leading exclamation mark
-    let splitCommand = fullCommand.split(" "); // Spliting the message into pieces
-    let primaryCommand = splitCommand[0];
-    let arguments = splitCommand.slice(1);
-
-    console.log("Command received: " + primaryCommand);
-
-    if (primaryCommand == "fart")   {
-        message.channel.send("You may want to check that you haven't followed through :poop:");
-    }
-}
 
 client.login(token);
