5dd8bcaf0839d73e20ccc5d90997330130730486
Justin Byrne <theskaterjesus@gmail.com>
2019-11-12 14:58:52 +0000

    Added a function to find the default channel

--- src/bot.js
+++ src/bot.js
@@ -34,4 +34,24 @@ client.on('message', (message) => {
     }
 })
 
+
+// create function to find default or first text channel
+const getDefaultChannel = (guild) => {
+    // get "original" default channel
+    if (guild.channels.has(guild.id)) return guild.channels.get(guild.id)
+
+    // Check for a "general" channel, which is often default chat
+    const generalChannel = guild.channels.find(channel => channel.name === "general");
+
+    if (generalChannel) return generalChannel;
+    // Now we get into the heavy stuff: first channel in order where the bot can speak
+    // hold on to your hats!
+    return guild.channels
+        .filter(c => c.type === "text" &&
+            c.permissionsFor(guild.client.user).has("SEND_MESSAGES"))
+        .sort((a, b) => a.position - b.position ||
+            Long.fromString(a.id).sub(Long.fromString(b.id)).toNumber())
+        .first();
+}
+
 client.login(token);
