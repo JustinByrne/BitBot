1a24e52244afaa78c16813ba20cb5f86cd9bcd54
Justin Byrne <JustinByrne@users.noreply.github.com>
2019-11-14 18:11:01 +0000

    Fixes #5

--- src/commands/fart.js
+++ src/commands/fart.js
@@ -2,7 +2,7 @@ const { prefix } = require('../config.json');
 
 module.exports = {
     name: 'fart',
-    description: 'Replies to the user who farted with a witty command.',
+    description: 'Replies to the user who farted with a witty comment.',
     command: prefix + 'fart',
     execute(message, args) {
         message.channel.send("You may want to check that you haven't followed through :poop:");
